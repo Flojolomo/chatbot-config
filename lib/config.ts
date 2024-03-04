55945a66770e242854136a4e8af50e66b95c342e
Flojolomo <48093713+Flojolomo@users.noreply.github.com>
2024-03-04 23:24:09 +0100

    feat: set up github action for diff &  deploy (#1)

---
+++ lib/config.ts
@@ -0,0 +1,8 @@
+import * as env from 'env-var';
+
+export const config = {
+  aws: {
+    region: env.get('AWS_REGION').required().asString(),
+    account: env.get('AWS_ACCOUNT_ID').required().asString()
+  },
+};
---
+++ lib/oidc-provider-stack.ts
@@ -0,0 +1,40 @@
+import {
+  GithubActionsIdentityProvider,
+  GithubActionsRole
+} from 'aws-cdk-github-oidc';
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+/**
+ * https://www.npmjs.com/package/aws-cdk-github-oidc
+ */
+export class OidcProviderStack extends cdk.Stack {
+  public constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const provider = new GithubActionsIdentityProvider(this, 'github-provider');
+
+    const deploymentRole = new GithubActionsRole(this, 'deployment-role', {
+      roleName: 'oidc-gha-deployment', // the role name
+      provider: provider, // reference into the OIDC provider
+      owner: 'Flojolomo', // your repository owner (organization or user) name
+      repo: 'chatbot-config' // your repository name (without the owner name)
+    });
+
+    new iam.ManagedPolicy(this, 'cdk-deploy-policy', {
+      roles: [deploymentRole],
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['sts:AssumeRole'],
+          resources: [`arn:aws:iam::${this.account}:role/cdk-*`],
+          effect: iam.Effect.ALLOW
+        })
+      ]
+    });
+
+    new cdk.CfnOutput(this, 'deployment-role-arn', {
+      value: deploymentRole.roleArn
+    });
+  }
+}
