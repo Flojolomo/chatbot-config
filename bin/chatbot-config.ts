a70710683f0b10a1f5b2f34472013a61a7149c23
Flojolomo <florian1siegel@googlemail.com>
2024-03-04 22:21:49 +0100

    feat: read variables from env

--- bin/chatbot-config.ts
+++ bin/chatbot-config.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ChatbotConfigStack } from '../lib/chatbot-config-stack';
 import { OidcProviderStack } from '../lib/oidc-provider-stack';
+import { config } from '../lib/config';
 
 const app = new cdk.App();
 
 new OidcProviderStack(app, 'OidcProviderStack', {
   env: {
-    region: 'eu-central-1',
-    account: '767397940951'
+    region: config.aws.region,
+    account: config.aws.account
   }
 });
