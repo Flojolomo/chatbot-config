c516bae2be35694551005622b71ec33585e277ea
Flojolomo <florian1siegel@googlemail.com>
2024-03-04 21:55:34 +0100

    feat: add policy to assume role

--- lib/oidc-provider-stack.ts
+++ lib/oidc-provider-stack.ts
@@ -3,6 +3,7 @@ import {
   GithubActionsRole
 } from 'aws-cdk-github-oidc';
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 /**
@@ -20,6 +21,13 @@ export class OidcProviderStack extends cdk.Stack {
       owner: 'Flojolomo', // your repository owner (organization or user) name
       repo: 'chatbot-config' // your repository name (without the owner name)
     });
+
+    new iam.ManagedPolicy(this, 'cdk-deploy-policy', {
+      roles: [deploymentRole],
+      statements: [
+        new iam.PolicyStatement({ actions: ['sts:AssumeRole'], resources: [`arn:aws:iam::${this.account}:role/cdk-*`], effect: iam.Effect.ALLOW }),
+      ]
+    })
 
     new cdk.CfnOutput(this, 'deployment-role-arn', {
       value: deploymentRole.roleArn
