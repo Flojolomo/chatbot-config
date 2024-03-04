087b398fd87781bc2e3bcbd168a9b57171bd762c
Flojolomo <florian1siegel@googlemail.com>
2024-03-04 23:41:40 +0100

    chore: fix format

--- lib/config.ts
+++ lib/config.ts
@@ -3,6 +3,7 @@ import * as env from 'env-var';
 export const config = {
   aws: {
     region: env.get('AWS_REGION').required().asString(),
-    account: env.get('AWS_ACCOUNT_ID').required().asString()
+    account: env.get('AWS_ACCOUNT_ID').required().asString(),
+  },
   },
 };
