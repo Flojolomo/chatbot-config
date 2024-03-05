dc8557863d15e4be387eb05cde32f6b79bc60cd2
Flojolomo <florian1siegel@googlemail.com>
2024-03-05 14:36:18 +0100

    add topic to notify slack

--- lib/chatbot-stack.ts
+++ lib/chatbot-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib/core';
 import * as chatbot from 'aws-cdk-lib/aws-chatbot';
 
 import { Construct } from 'constructs';
+import * as sns from 'aws-cdk-lib/aws-sns';
 
 interface ChatbotStackProps extends cdk.StackProps {
   slack: {
@@ -15,10 +16,13 @@ export class ChatbotStack extends cdk.Stack {
   public constructor(scope: Construct, id: string, props: ChatbotStackProps) {
     super(scope, id, props);
 
+    const chatbotTopic = new sns.Topic(this, 'chatbot-topic');
+
     new chatbot.SlackChannelConfiguration(this, 'slack-channel-configuration', {
       slackChannelConfigurationName: props.slack.channelConfigurationName,
       slackChannelId: props.slack.channelId,
       slackWorkspaceId: props.slack.workspaceId,
+      notificationTopics: [chatbotTopic],
     });
   }
 }
