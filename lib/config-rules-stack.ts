e6d82b56170c744bc8fa7ba50cc1bae4b3acafb6
Flojolomo <florian1siegel@googlemail.com>
2024-03-05 22:24:36 +0100

    set up config delivery to enable config rules

--- lib/config-rules-stack.ts
+++ lib/config-rules-stack.ts
@@ -1,7 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import * as config from 'aws-cdk-lib/aws-config';
 import * as sns from 'aws-cdk-lib/aws-sns';
+
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3'; // Import the 's3' module from 'aws-cdk-lib'
 
 interface ConfigRuleStackProps extends cdk.StackProps {
   cloudformationNotificationTopics: sns.ITopic[];
@@ -15,6 +18,8 @@ export class ConfigRuleStack extends cdk.Stack {
   ) {
     super(scope, id, props);
 
+    this.setUpConfigService();
+
     new config.CloudFormationStackNotificationCheck(
       this,
       'cloudformation-stack-notification',
@@ -22,5 +27,70 @@ export class ConfigRuleStack extends cdk.Stack {
         topics: props.cloudformationNotificationTopics,
       },
     );
+  }
+
+  private createConfigBucket(role: iam.IRole): s3.IBucket {
+    const bucket = new s3.Bucket(this, 'ConfigBucket');
+
+    // Attaches the AWSConfigBucketPermissionsCheck policy statement.
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        principals: [role],
+        resources: [bucket.bucketArn],
+        actions: ['s3:GetBucketAcl'],
+      }),
+    );
+
+    // Attaches the AWSConfigBucketDelivery policy statement.
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        principals: [role],
+        resources: [
+          bucket.arnForObjects(
+            `AWSLogs/${cdk.Stack.of(this).account}/Config/*`,
+          ),
+        ],
+        actions: ['s3:PutObject'],
+        conditions: {
+          StringEquals: {
+            's3:x-amz-acl': 'bucket-owner-full-control',
+          },
+        },
+      }),
+    );
+
+    return bucket;
+  }
+
+  private createRoleForConfigService(): iam.IRole {
+    return new iam.Role(this, 'config-role', {
+      assumedBy: new iam.ServicePrincipal('config.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'service-role/AWSConfigRole',
+        ),
+      ],
+    });
+  }
+
+  private createConfigRecorder(role: iam.IRole): void {
+    new config.CfnConfigurationRecorder(this, 'ConfigRecorder', {
+      roleArn: role.roleArn,
+      recordingGroup: {
+        allSupported: true,
+      },
+    });
+  }
+
+  private setUpConfigService(): void {
+    const role = this.createRoleForConfigService();
+    this.createConfigRecorder(role);
+    const configBucket = this.createConfigBucket(role);
+
+    new config.CfnDeliveryChannel(this, 'ConfigDeliveryChannel', {
+      s3BucketName: configBucket.bucketName,
+    });
   }
 }
