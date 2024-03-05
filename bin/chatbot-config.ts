5f97029d903ef3439cb4743715734c53ed4f3f3d
Flojolomo <florian1siegel@googlemail.com>
2024-03-05 01:09:13 +0100

    integrate chatbot stack

--- bin/chatbot-config.ts
+++ bin/chatbot-config.ts
@@ -3,12 +3,21 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { OidcProviderStack } from '../lib/oidc-provider-stack';
 import { config } from '../lib/config';
+import { ChatbotStack } from '../lib/chatbot-stack';
 
 const app = new cdk.App();
 
 new OidcProviderStack(app, 'OidcProviderStack', {
   env: {
     region: config.aws.region,
-    account: config.aws.account
-  }
+    account: config.aws.account,
+  },
+});
+
+new ChatbotStack(app, 'ChatbotStack', {
+  env: {
+    region: config.aws.region,
+    account: config.aws.account,
+  },
+  slack: config.chatbot.slack,
 });
--- lib/config.ts
+++ lib/config.ts
@@ -5,4 +5,14 @@ export const config = {
     region: env.get('AWS_REGION').required().asString(),
     account: env.get('AWS_ACCOUNT_ID').required().asString(),
   },
+  chatbot: {
+    slack: {
+      channelConfigurationName: env
+        .get('SLACK_CHANNEL_CONFIGURATION_NAME')
+        .required()
+        .asString(),
+      channelId: env.get('SLACK_CHANNEL_ID').required().asString(),
+      workspaceId: env.get('SLACK_WORKSPACE_ID').required().asString(),
+    },
+  },
 };
