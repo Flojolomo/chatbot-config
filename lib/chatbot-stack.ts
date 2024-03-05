676a28fd4a4a997fbd6e8f501861db15110670d0
Flojolomo <48093713+Flojolomo@users.noreply.github.com>
2024-03-05 14:43:23 +0100

    fix: add sns topic for notifications (#6)

--- lib/chatbot-stack.ts
+++ lib/chatbot-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib/core';
 import * as chatbot from 'aws-cdk-lib/aws-chatbot';
 
 import { Construct } from 'constructs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 interface ChatbotStackProps extends cdk.StackProps {
   slack: {
@@ -15,10 +17,14 @@ export class ChatbotStack extends cdk.Stack {
   public constructor(scope: Construct, id: string, props: ChatbotStackProps) {
     super(scope, id, props);
 
+    const chatbotTopic = new sns.Topic(this, 'chatbot-topic');
+
     new chatbot.SlackChannelConfiguration(this, 'slack-channel-configuration', {
       slackChannelConfigurationName: props.slack.channelConfigurationName,
       slackChannelId: props.slack.channelId,
       slackWorkspaceId: props.slack.workspaceId,
+      notificationTopics: [chatbotTopic],
+      logRetention: logs.RetentionDays.TWO_WEEKS,
     });
   }
 }
