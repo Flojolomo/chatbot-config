a0a9f7da70b8a368a1d75aa7232d9064cca352b9
Flojolomo <florian1siegel@googlemail.com>
2024-03-11 20:33:48 +0100

    add dynamodb table

--- lib/use-cases/x-ray-tracing/x-ray-tracing-stack.ts
+++ lib/use-cases/x-ray-tracing/x-ray-tracing-stack.ts
@@ -5,6 +5,7 @@ import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as sqs from 'aws-cdk-lib/aws-sqs'; // Import the missing sqs module
 import * as sns from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class XRayTracingStack extends cdk.Stack {
   public constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,5 +16,6 @@ export class XRayTracingStack extends cdk.Stack {
     new sqs.Queue(this, 'queue', {});
     new lambda.NodejsFunction(this, 'lambda', {});
     new sns.Topic(this, 'topic', {});
+    new dynamodb.Table(this, 'table', {});
   }
 }
