cc2ece9524d01290a0f332fc32b7ad8f501291f5
Flojolomo <florian1siegel@googlemail.com>
2024-03-11 20:16:53 +0100

    move oidc stack to services/iam

--- bin/chatbot-config.ts
+++ bin/chatbot-config.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { OidcProviderStack } from '../lib/oidc-provider-stack';
+import { OidcProviderStack } from '../lib/services/iam/oidc-provider-stack';
 import { config } from '../lib/config';
 import { ChatbotStack } from '../lib/chatbot-stack';
 import { ConfigRuleStack } from '../lib/config-rules-stack';
