319679668eeffabe5fc2e087e6d91259ffb14c68
Flojolomo <florian1siegel@googlemail.com>
2024-03-11 20:32:21 +0100

    create scaffold of resources

--- lib/use-cases/x-ray-tracing/x-ray-tracing-stack.ts
+++ lib/use-cases/x-ray-tracing/x-ray-tracing-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import * as apiGateway from 'aws-cdk-lib/aws-apigatewayv2';
+import * as events from 'aws-cdk-lib/aws-events';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as sqs from 'aws-cdk-lib/aws-sqs'; // Import the missing sqs module
+import * as sns from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 
 export class XRayTracingStack extends cdk.Stack {
   public constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // API Gateway
-    new apiGateway.HttpApi(this, 'HttpApi', {});
-    // EventBridge
-    // SQS
-    // Lambda
-    // SNS
+    new apiGateway.HttpApi(this, 'http-api', {});
+    new events.EventBus(this, 'event-bus', {});
+    new sqs.Queue(this, 'queue', {});
+    new NodejsFunction(this, 'lambda', {});
+    new sns.Topic(this, 'topic', {});
   }
 }
