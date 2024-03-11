28eb0fe33b0ea7434b828af11ef5b2e4fc2bbbfd
Flojolomo <48093713+Flojolomo@users.noreply.github.com>
2024-03-11 20:24:23 +0100

    refactor: directory structure to split according to service (#13)

--- bin/chatbot-config.ts
+++ bin/chatbot-config.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { OidcProviderStack } from '../lib/oidc-provider-stack';
+import { OidcProviderStack } from '../lib/services/iam/oidc-provider-stack';
 import { config } from '../lib/config';
-import { ChatbotStack } from '../lib/chatbot-stack';
+import { ChatbotStack } from '../lib/services/chatbot/chatbot-stack';
 import { ConfigStack } from '../lib/services/config/config-stack';
 
 const app = new cdk.App();
