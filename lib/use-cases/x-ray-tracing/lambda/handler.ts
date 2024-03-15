3a829c7fe77b1ffd48c9762093598aa2b9279e67
Flojolomo <florian1siegel@googlemail.com>
2024-03-15 20:48:55 +0100

    set up batch processing in lambda handler

--- lib/use-cases/x-ray-tracing/lambda/handler.ts
+++ lib/use-cases/x-ray-tracing/lambda/handler.ts
@@ -2,17 +2,34 @@ import { Tracer } from '@aws-lambda-powertools/tracer';
 import { LambdaInterface } from '@aws-lambda-powertools/commons/types';
 
 import { Logger } from '@aws-lambda-powertools/logger';
+import {
+  BatchProcessor,
+  EventType,
+  processPartialResponse,
+} from '@aws-lambda-powertools/batch';
+import { SQSEvent, SQSRecord } from 'aws-lambda';
 
 const logger = new Logger({});
 const tracer = new Tracer({});
+
+const processor = new BatchProcessor(EventType.SQS);
 
 class Lambda implements LambdaInterface {
   // Set the log event flag to true
   @tracer.captureLambdaHandler()
   @logger.injectLambdaContext({ logEvent: true })
   // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-  public async handler(_event: unknown, _context: unknown): Promise<void> {
+  public async handler(_event: SQSEvent, _context: unknown): Promise<void> {
     logger.info('This is an INFO log with some context');
+    await processPartialResponse(
+      _event,
+      this.recordHandler.bind(this),
+      processor,
+    );
+  }
+
+  private async recordHandler(record: SQSRecord) {
+    logger.info('Processing event', { record });
   }
 }
 
