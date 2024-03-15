58b6d8d4fdaa0dd9a0606347999036660c276c71
Flojolomo <florian1siegel@googlemail.com>
2024-03-15 20:14:07 +0100

    set up missing resources

--- lib/use-cases/x-ray-tracing/x-ray-tracing-stack.ts
+++ lib/use-cases/x-ray-tracing/x-ray-tracing-stack.ts
@@ -6,6 +6,11 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as eventTargets from 'aws-cdk-lib/aws-events-targets';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs';
+import path = require('path');
+import * as sns from 'aws-cdk-lib/aws-sns'; // Import the missing sns module
 
 // Import the missing iam module
 // TODO open API Spec
@@ -26,23 +31,23 @@ export class XRayTracingStack extends cdk.Stack {
     const { eventBus } = this.createEventBus();
     this.forwardPostRequests(api, eventBus);
 
-    // new sqs.Queue(this, 'queue', {});
+    new sqs.Queue(this, 'queue', {});
 
-    // new lambdaNodeJs.NodejsFunction(this, 'lambda', {
-    //   entry: path.join(__dirname, 'lambda', 'handler.ts'),
-    //   environment: {
-    //     POWERTOOLS_SERVICE_NAME: 'x-ray-showcase',
-    //     POWERTOOLS_TRACE_ENABLED: String(true),
-    //     POWERTOOLS_TRACER_CAPTURE_HTTPS_REQUESTS: String(true),
-    //     POWERTOOLS_TRACER_CAPTURE_RESPONSE: String(true),
-    //     POWERTOOLS_TRACER_CAPTURE_ERROR: String(true),
-    //     POWERTOOLS_LOG_LEVEL: 'DEBUG',
-    //   },
-    //   runtime: lambda.Runtime.NODEJS_20_X,
-    //   tracing: lambda.Tracing.ACTIVE,
-    // });
+    new lambdaNodeJs.NodejsFunction(this, 'lambda', {
+      entry: path.join(__dirname, 'lambda', 'handler.ts'),
+      environment: {
+        POWERTOOLS_SERVICE_NAME: 'x-ray-showcase',
+        POWERTOOLS_TRACE_ENABLED: String(true),
+        POWERTOOLS_TRACER_CAPTURE_HTTPS_REQUESTS: String(true),
+        POWERTOOLS_TRACER_CAPTURE_RESPONSE: String(true),
+        POWERTOOLS_TRACER_CAPTURE_ERROR: String(true),
+        POWERTOOLS_LOG_LEVEL: 'DEBUG',
+      },
+      runtime: lambda.Runtime.NODEJS_20_X,
+      tracing: lambda.Tracing.ACTIVE,
+    });
 
-    // new sns.Topic(this, 'topic', {});
+    new sns.Topic(this, 'topic', {});
     new dynamodb.Table(this, 'table', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       removalPolicy: cdk.RemovalPolicy.DESTROY,
