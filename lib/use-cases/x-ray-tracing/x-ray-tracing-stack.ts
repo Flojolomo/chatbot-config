d4eb8ac9f0db5dcc29250fd13aff65717cd06b0e
Flojolomo <florian1siegel@googlemail.com>
2024-03-17 20:39:51 +0100

    add comment

--- lib/use-cases/x-ray-tracing/x-ray-tracing-stack.ts
+++ lib/use-cases/x-ray-tracing/x-ray-tracing-stack.ts
@@ -40,7 +40,7 @@ export class XRayTracingStack extends cdk.Stack {
     api.integrateLambda({
       lambdaFunction: apiRequestHandler,
       method: 'POST',
-      path: 'lambda-event',
+      path: 'lambda-event', // Should be /lambda-event
     });
 
     // this.createBufferedProcessingPipeline({ eventBus });
